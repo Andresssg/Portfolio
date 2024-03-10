3200540dbe87a2870e896439ff953784e5e2c846
Andres Guevara <andres-guevara1@upc.edu.co>
2024-03-10 17:05:17 -0500

    Add tech pill to show tech icons

---
+++ src/components/TechPill.jsx
@@ -0,0 +1,20 @@
+import ICONS from '../icons/IconsIndex'
+
+function TechPill ({ name }) {
+  const iconObj = ICONS[name] || null
+  const Icon = iconObj?.component || null
+  const url = iconObj?.url || '#'
+  return (
+    <a
+      href={url}
+      target='_blank'
+      rel='noreferrer'
+      className='flex items-center gap-x-2 px-3 py-2 text-xs font-semibold uppercase rounded-full border-2 border-primary-light hover:scale-125 duration-300 hover:bg-primary-light hover:text-secondary-light'
+    >
+      {Icon && <Icon className='w-4 h-4' />}
+      {name}
+    </a>
+  )
+}
+
+export default TechPill
