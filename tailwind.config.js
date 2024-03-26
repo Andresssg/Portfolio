0ccf9a4ac22de9394b960eed7b81844b26d107c5
Andres Guevara <andres-guevara1@upc.edu.co>
2024-03-26 01:11:25 -0500

    Add typewriter animation and accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,28 @@ export default {
         rotate: {
           from: { transform: 'rotate(0deg)' },
           to: { transform: 'rotate(360deg)' }
+        },
+        typewriter: {
+          to: {
+            left: '100%'
+          }
+        },
+        blink: {
+          '0%': {
+            opacity: '0'
+          },
+          '50%': {
+            opacity: '1'
+          },
+          '100%': {
+            opacity: '0'
+          }
         }
       },
       animation: {
-        rotate: 'rotate 3.5s linear infinite'
+        rotate: 'rotate 3.5s linear infinite',
+        typewriter: 'typewriter 3s steps(20) forwards',
+        caret: 'typewriter 3s steps(20) forwards, blink 1s steps(11) infinite 2s'
       },
       boxShadow: {
         custom: '0px 10px 10px -10px rgba(0, 0, 0, 0.17)'
@@ -22,6 +40,7 @@ export default {
       colors: {
         primary: '#1e1f26',
         secondary: colors.slate[800],
+        accent: '#e2e2e2',
         github: '#211F1F',
         linkedin: '#0077B5'
       },
