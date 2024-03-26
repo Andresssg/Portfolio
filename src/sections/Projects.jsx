ab41a2e702fa34106755d4d18a58570fa4babcdf
Andres Guevara <andres-guevara1@upc.edu.co>
2024-03-26 01:18:31 -0500

    change colors

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -10,7 +10,7 @@ function Projects () {
     setShowModal(true)
   }
   return (
-    <section id='projects' className='flex flex-col w-full px-8 py-10 bg-secondary text-primary gap-y-10 md:px-2'>
+    <section id='projects' className='flex flex-col w-full px-8 py-10 text-accent gap-y-10 md:px-2'>
       <h1 className='font-bold text-4xl md:text-5xl text-center'>Proyectos</h1>
       <div className='flex flex-wrap w-full justify-evenly xl:justify-between gap-y-8'>
         {data.map(project => {
