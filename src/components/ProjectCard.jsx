66821cbfa313cdec2bff80c0160331e7b9816d11
Andres Guevara <andres-guevara1@upc.edu.co>
2024-03-26 01:19:22 -0500

    Add group, change colors and add aspect ratio

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,14 +5,14 @@ function ProjectCard ({ info, onClick }) {
   return (
     <article
       title='Mostrar mas' className='project-card flex flex-col relative gap-y-2 p-4 w-full justify-center rounded-xl
-      overflow-hidden cursor-pointer md:w-[40ch] lg:w-[50ch]  shadow-lg border-4 border-slate-700'
+      overflow-hidden cursor-pointer md:w-[40ch] lg:w-[50ch] shadow-lg border-4 border-slate-700 group'
       onClick={onClick}
     >
       <header className='flex flex-col w-full justify-between gap-y-2 z-10'>
         <img
           src={mainImage} alt={`Imagen del proyecto ${name}`}
           loading='lazy'
-          className='object-cover h-48 w-full rounded-lg'
+          className='object-cover h-48 w-full rounded-lg aspect-video'
         />
         <h3 className='font-bold text-2xl flex-grow'>{name}</h3>
       </header>
@@ -24,8 +24,8 @@ function ProjectCard ({ info, onClick }) {
               Icon &&
                 <span
                   key={`pc-${tech}`}
-                  title={tech} className='-mr-5 p-1.5 bg-secondary rounded-full border-2 border-slate-500
-                  hover:scale-110 hover:mr-0 hover:shadow-custom hover:shadow-primary duration-200'
+                  title={tech} className='-mr-5 p-1.5 bg-primary rounded-full border-2 border-slate-500
+                  hover:scale-110 hover:mr-0 hover:shadow-custom hover:shadow-primary duration-200 group-hover:bg-secondary'
                 >
                   <Icon
                     className='w-6 h-6'
