71fc53523d3b7aeba7d7475ba81607f58c4e5ad3
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-02 16:05:59 -0500

    Delete modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,15 @@ import Hero from './sections/Hero'
 import Background from './sections/Background'
 import Projects from './sections/Projects'
 import Footer from './sections/Footer'
-import Modal from './sections/Modal'
-import ModalContextProvider from './contexts/ModalContext'
 
 function App () {
   return (
     <div className='flex flex-col min-h-screen items-center content-center bg-primary'>
-      <ModalContextProvider>
-        <NavBar />
-        <Hero />
-        <Background />
-        <Projects />
-        <Footer />
-        <Modal />
-      </ModalContextProvider>
+      <NavBar />
+      <Hero />
+      <Background />
+      <Projects />
+      <Footer />
     </div>
   )
 }
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,21 +1,14 @@
 import ProjectCard from '../components/ProjectCard'
 import data from '../data/projects-info.json'
-import { ModalContext } from '../contexts/ModalContext'
-import { useContext } from 'react'
 
 function Projects () {
-  const { setShowModal, setProjectInfo } = useContext(ModalContext)
-  const handleCardClick = (projectInfo) => {
-    setProjectInfo(projectInfo)
-    setShowModal(true)
-  }
   return (
     <section id='projects' className='flex flex-col w-full px-8 py-10 text-accent gap-y-10 md:px-2'>
       <h1 className='font-bold text-4xl md:text-5xl text-center'>Proyectos</h1>
       <div className='flex flex-wrap w-full justify-evenly xl:justify-between gap-y-8'>
         {data.map(project => {
           return (
-            <ProjectCard key={`p-${project.id}`} info={project} onClick={() => handleCardClick(project)} />
+            <ProjectCard key={`p-${project.id}`} info={project} />
           )
         })}
       </div>
