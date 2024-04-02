a8c45d9f4d26f9ebc98db07364b13e7de7cf2257
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-02 17:49:00 -0500

    Add mail icon and change styles

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,18 +1,20 @@
 import Github from '../icons/Github'
 import LinkedIn from '../icons/Linkedin'
+import Mail from '../icons/Mail'
 
 function Hero () {
   return (
     <section className='flex flex-col w-full h-screen p-14 pb-16 justify-end items-left
-    text-accent
+    text-light-gray
     '
     >
-      <h1 className='font-bold text-6xl lg:text-8xl typewriter'>Andres Guevara</h1>
+      <h1 className='font-bold text-6xl lg:text-8xl typewriter text-accent'>Andres Guevara</h1>
       <h2 className='font-semibold text-3xl lg:text-4xl'>Ingeniero de Sistemas</h2>
       <h3 className='font-medium text-xl lg:text-2xl'>Desarrollador fullstack</h3>
-      <div className='flex gap-2 mt-2 md:gap-5'>
-        <Github className='w-7 md:w-12 hover:scale-150 hover:text-white duration-300' />
-        <LinkedIn className='w-7 md:w-12 hover:scale-150 hover:text-linkedin duration-300' />
+      <div className='flex gap-2 mt-2 md:gap-5 items-center'>
+        <Github className='w-7 md:w-12 hover:scale-110 hover:text-accent duration-300' />
+        <LinkedIn className='w-7 md:w-12 hover:scale-110 hover:text-accent duration-300' />
+        <Mail className='w-7 md:w-14 hover:scale-110 hover:text-accent duration-300' />
       </div>
     </section>
   )
