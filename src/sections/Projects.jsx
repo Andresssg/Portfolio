1f35aab5c69a2db82d3f409158c92a8c1182ef1c
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-02 16:46:48 -0500

    Change styles

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -5,7 +5,7 @@ function Projects () {
   return (
     <section id='projects' className='flex flex-col w-full px-8 py-10 text-accent gap-y-10 md:px-2'>
       <h1 className='font-bold text-4xl md:text-5xl text-center'>Proyectos</h1>
-      <div className='flex flex-wrap w-full justify-evenly xl:justify-between gap-y-8'>
+      <div className='flex flex-col md:flex-row md:flex-wrap w-full justify-around gap-y-8'>
         {data.map(project => {
           return (
             <ProjectCard key={`p-${project.id}`} info={project} />
