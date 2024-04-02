586b94145e8c689368ccfdf08c83c81189bd72ff
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-02 18:54:48 -0500

    Use keyword component and destructure experience obj

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,20 @@
 import useDiffDates from '../hooks/useDiffDates'
 import Suitcase from '../icons/Suitcase'
+import Keywords from './Keywords'
 
 function Experience ({ experience }) {
-  const { startLocaleDate, endLocaleDate, amountMonths } = useDiffDates(experience.start, experience.end)
+  const { title, company, description, start, end } = experience
+  const { startLocaleDate, endLocaleDate, amountMonths } = useDiffDates(start, end)
+
   return experience
     ? (
       <article className='flex flex-col my-3 w-full mx-auto px-6 container lg:max-w-4xl md:max-w-2xl lg:gap-y-5 justify-center items-left '>
         <div className='flex items-center gap-2 text-2xl lg:text-4xl'>
           <Suitcase className='w-10 h-10' />
-          <h2 className='font-bold'>{experience.title}</h2>
+          <h2 className='font-bold'>{title}</h2>
         </div>
         <div className='mt-2 ml-5 py-2 px-6 font-semibold text-xl lg:text-2xl border-light-gray border-l-4'>
-          <p className=''>{experience.company}</p>
+          <p className=''>{company}</p>
           <div className='flex capitalize'>
             <div className='flex gap-x-2 pr-3 items-center border-r-2 border-light-gray'>
               <p>{startLocaleDate}</p>
@@ -21,7 +24,7 @@ function Experience ({ experience }) {
             </div>
             <p className='px-3'>{amountMonths} meses</p>
           </div>
-          <p className='font-normal text-lg lg:text-xl'>{experience.description}</p>
+          <Keywords description={description} />
         </div>
       </article>
       )
