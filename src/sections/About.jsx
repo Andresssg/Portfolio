c020f0af0f28eca727692def1f48086400025c27
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-03 19:18:52 -0500

    Add about component

---
+++ src/sections/About.jsx
@@ -0,0 +1,14 @@
+function About () {
+  return (
+    <section
+      id='about' className='flex flex-col px-4 my-5 justify-center items-center text-light-gray
+      md:my-16 lg:max-w-7xl text-xl'
+    >
+      <h1 className='py-20 font-bold text-4xl md:text-5xl text-accent text-center'>Sobre mi</h1>
+      <p className='md:w-[60ch]'>Sobre mi
+      </p>
+    </section>
+  )
+}
+
+export default About
