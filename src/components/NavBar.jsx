a01a486b6f53354da41507475e5373d7f3ea1721
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-03 19:19:06 -0500

    Add about component to app and navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ function NavBar () {
     <nav className='fixed top-0 flex justify-end gap-x-6 w-full px-8 py-4 font-semibold
     backdrop-blur-lg text-light-gray shadow-lg z-50 text-xl'
     >
+      <a className='hover:scale-125 hover:text-accent duration-300' href='#about'>Sobre mi</a>
       <a className='hover:scale-125 hover:text-accent duration-300' href='#background'>Experiencia</a>
       <a className='hover:scale-125 hover:text-accent duration-300' href='#projects'>Proyectos</a>
-      <a className='hover:scale-125 hover:text-accent duration-300' href='#footer'>Footer</a>
     </nav>
   )
 }
