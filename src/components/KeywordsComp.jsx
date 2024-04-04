68d8cf8c77e02575c50b3ebb079bcd68246576b6
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-03 19:04:18 -0500

    Add more keywords and Order by length

--- src/components/KeywordsComp.jsx
+++ src/components/KeywordsComp.jsx
@@ -1,12 +1,13 @@
 import { keywords } from '../hooks/keywords'
-function KeywordsComp ({ className = '', description = '' }) {
-  const regex = new RegExp(`(${keywords.join('|')})`, 'gi')
-  const descriptionFragments = description.split(regex)
 
+function KeywordsComp ({ className = '', description = '' }) {
+  const sortedKeywords = keywords.sort((a, b) => b.length - a.length)
+  const regex = new RegExp(`(${sortedKeywords.join('|')})`, 'gi')
+  const descriptionFragments = description.split(regex)
   return (
     <p className={className}>
       {descriptionFragments.map((fragment, index) => {
-        const isKeyword = keywords.includes(fragment)
+        const isKeyword = sortedKeywords.includes(fragment)
         return (
           <span key={index} className={isKeyword ? 'text-accent font-bold' : ''}>
             {fragment}
--- src/hooks/keywords.js
+++ src/hooks/keywords.js
@@ -1,6 +1,9 @@
 export const keywords = [
   'Java', 'Spring Boot', 'Angular',
-  'NodeJS', 'React', 'SCRUM',
-  'APIs', 'IA', 'Inteligencia Artificial',
-  'Aplicación web', 'aplicación web'
+  'JavaScript', 'TypeScript', 'HTML',
+  'CSS', 'Tailwind', 'SQL', 'MySQL',
+  'PostgreSQL', 'MongoDB', 'NodeJS',
+  'React', 'SCRUM', 'APIs',
+  'IA', 'Inteligencia Artificial', 'Aplicación web',
+  'aplicación web'
 ]
