69a94e3b706f914afa1808800a3de412924ba4ac
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-03 21:25:19 -0500

    Hide Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NavBar from './components/NavBar'
 import Hero from './sections/Hero'
 import Background from './sections/Background'
 import Projects from './sections/Projects'
-import Footer from './sections/Footer'
+/* import Footer from './sections/Footer' */
 import About from './sections/About'
 
 function App () {
@@ -13,7 +13,7 @@ function App () {
       <About />
       <Background />
       <Projects />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
