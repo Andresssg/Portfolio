ede49f18634b26c58b927fefb10cd017357fea4b
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-03 22:25:46 -0500

    Fix property stopcolor

--- src/icons/Tailwind.jsx
+++ src/icons/Tailwind.jsx
@@ -10,8 +10,8 @@ function Tailwind ({ className = '' }) {
     >
       <defs>
         <linearGradient x1='-2.778%' y1='32%' x2='100%' y2='67.556%' id='gradient'>
-          <stop stop-color='#2298BD' offset='0%' />
-          <stop stop-color='#0ED7B5' offset='100%' />
+          <stop stopColor='#2298BD' offset='0%' />
+          <stop stopColor='#0ED7B5' offset='100%' />
         </linearGradient>
       </defs>
       <path
