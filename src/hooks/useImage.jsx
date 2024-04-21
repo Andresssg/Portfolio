d9e3a16d2bec55da0f1ca27a6e441cfd3b069cb8
Andres Guevara <andres-guevara1@upc.edu.co>
2024-04-21 01:38:28 -0500

    Add reach it image

--- src/hooks/useImage.jsx
+++ src/hooks/useImage.jsx
@@ -1,8 +1,9 @@
 import cesp from '../assets/cesp.webp'
 import portfolio from '../assets/portfolio.webp'
+import reachIt from '../assets/reach-it.webp'
 
 function useImage (name) {
-  const images = [cesp, portfolio]
+  const images = [cesp, portfolio, reachIt]
   const [baseName, ext] = name.split('.')
   const img = images.find(img => new RegExp(`${baseName}.*\\.${ext}`).test(img))
   return img
