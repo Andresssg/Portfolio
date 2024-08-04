a0dfc0168328960df56292f16da095ba204cc465
Andres Guevara <guevaraandres7@gmail.com>
2024-08-04 10:05:38 -0500

    Initial astro template

--- src/hooks/useModalContext.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-import { ModalContext } from '../contexts/ModalContext'
-
-export default function useModalContext () {
-  const { showModal, setShowModal } = useContext(ModalContext)
-  return { showModal, setShowModal }
-}
