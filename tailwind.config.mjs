5112c99a9ebb79728938a7fa5add8e0e25892415
Andres Guevara <guevaraandres7@gmail.com>
2024-08-04 12:27:52 -0500

    Add custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#1e1f26',
+				'secondary': '#22d3ee',
+			},
+		},
 	},
 	plugins: [],
 }
